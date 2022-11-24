a5a52a68789d91a04e9c00399b04d8d8741b3375
Wuido1010 <gu.altamirano@duocuc.cl>
2022-11-23 21:32:39 -0300

    probando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Componente{
+  icon:string;
+  name:string;
+  redirecTo:string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,4 +13,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+
+
+  componentes: Componente[] = [ 
+    {
+      icon: 'wifi-outline',
+      name: 'Inicio',
+      redirecTo: '/inicio',
+    },
+    {
+      icon: 'book-outline',
+      name: 'Asignaturas',
+      redirecTo:'/action-sheet',
+    },
+    {
+      icon: 'albums-outline',
+      name: 'Información',
+      redirecTo:'/card',
+    },
+    {
+      icon: 'walk-outline',
+      name: 'In',
+      redirecTo:'/inputs',
+    },
+    {
+      icon: 'eye-outline',
+      name: 'Noticias',
+      redirecTo:'/noticias',
+    },
+    {
+      icon: 'diamond-outline',
+      name: 'Comentarios',
+      redirecTo:'/comentarios',
+    }, 
+    {
+      icon: 'person-outline',
+      name: 'Mascotas',
+      redirecTo:'/datos',
+    },
+  ];
+
+
+
+
 }
